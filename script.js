93945fc3a32255fb4f9290da899075c4e51102eb
Mateusz Ramotowski <mateusz.ramotowski.praca@gmail.com>
2022-03-19 18:22:59 +0100

    Changing number of divs with buttons - changing inner html. Beginning of creating a board to play

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ function names: use words get, change, delete, create, add, return
 from begging write clean code, do small code refactoring
 use chrome debugger for finding bugs*/
 //alert("It is connected");  // It works
+const changeBlocksArea = document.getElementsByClassName("row my-2")[0];
+const changeBlocksButtonMakeSmallerArea = document.getElementsByTagName("button")[0];
+const changeBlocksButtonMakeBiggerrArea = document.getElementsByTagName("button")[1];
+
+
+//let HTMLInsidechangeBlocksArea = changeBlocksArea.innerHTML;
+//console.log(HTMLInsidechangeBlocksArea);
+
+changeBlocksButtonMakeSmallerArea.addEventListener("click", function(){
+    changeBlocksArea.innerHTML = `
+    <div class="col mx-1">column</div>
+          <div class="col mx-1">Column</div>
+          <div class="col mx-1">Column</div>
+          <div class="col mx-1">Column</div> 
+    `
+});
+changeBlocksButtonMakeBiggerrArea.addEventListener("click", function(){
+    changeBlocksArea.innerHTML = `
+    <div class="col mx-1">column</div>
+    <div class="col mx-1">Column</div>
+    <div class="col mx-1">Column</div>
+    <div class="col mx-1">Column</div>
+    <div class="col mx-1">column</div>
+    <div class="col mx-1">Column</div>
+    <div class="col mx-1">column</div>
+    <div class="col mx-1">Column</div>
+    <div class="col mx-1">column</div>
+    `
+})
+
+          
