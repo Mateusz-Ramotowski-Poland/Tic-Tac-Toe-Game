90766b65022c8345aaca39df43544c32fe13c8b9
Mateusz Ramotowski <mateusz.ramotowski.praca@gmail.com>
2022-03-19 15:20:15 +0100

    Notes from previous project as comments. In Index html I used bootstrap basic page template

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+/* If You need use pseudo code or algorithms 
+Use camelCase for variable name. Think about system for variable names. 
+function names: use words get, change, delete, create, add, return
+from begging write clean code, do small code refactoring
+use chrome debugger for finding bugs*/
+//alert("It is connected");  // It works
