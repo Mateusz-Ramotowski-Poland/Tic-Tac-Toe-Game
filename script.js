5a91035c7444fd4986d06ba55db6acd45b8e7bbb
Mateusz Ramotowski <mateusz.ramotowski.praca@gmail.com>
2022-03-21 21:25:39 +0100

    I can change size of grid to play. Now I am doing it using one short,elegant function. Next thing to do: When I will click a grid box draw X on grid box

--- script.js
+++ script.js
@@ -1,143 +1,28 @@
 "use strict";
+// name of function, last word: DOM - change property of DOM element, it is visible on webpage;
+
 /* If You need use pseudo code or algorithms 
 Use camelCase for variable name. Think about system for variable names. 
 function names: use words get, change, delete, create, add, return
 from begging write clean code, do small code refactoring
 use chrome debugger for finding bugs*/
 //alert("It is connected");  // It works
-const changeBlocksArea = document.getElementsByClassName("change")[0];
-const changeBlocks3x3 = document.getElementsByTagName("button")[0];
-const changeBlocks5x5 = document.getElementsByTagName("button")[1];
-const changeBlocks7x7 = document.getElementsByTagName("button")[2];
+const gridArea = document.getElementsByClassName("grid-area")[0];
+const buttonsChangeGridSize = document.getElementsByTagName("button"); // there are three buttons
 
-changeBlocks3x3.addEventListener("click", function(){
-    changeBlocksArea.innerHTML = `
-    <section class="change">
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    `
-});
-changeBlocks5x5.addEventListener("click", function(){
-    changeBlocksArea.innerHTML = `
-    <section class="change">
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    `
-})
-changeBlocks7x7.addEventListener("click", function(){
-    changeBlocksArea.innerHTML = `
-    <section class="change">
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div>
-    <div class="row">
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-      <div class="board-game-area"></div>
-    </div> 
-    </div>
-    `
-})
+function drawBoardToPLayDOM(event) {
+  const numberOrRowsColumns = event.currentTarget.innerText[0]; //first char from clicked button 
+  let innerHTML = "";
+  for (let i = 0; i < numberOrRowsColumns; i++) {
+    innerHTML+='\n<div class="row">\n'
+    for (let i = 0; i < numberOrRowsColumns; i++) {
+      innerHTML+='  <div class="board-game-area"></div>\n';
+    }
+    innerHTML+='</div>\n';
+  }
+  gridArea.innerHTML = innerHTML;
+}
+for(let buttonChangeGridSize of buttonsChangeGridSize){
+  buttonChangeGridSize.addEventListener("click", drawBoardToPLayDOM);
+}
 
-          
