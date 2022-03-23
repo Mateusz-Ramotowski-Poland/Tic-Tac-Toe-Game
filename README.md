1f2f255579f54cd5719c6890207d6b2524f92a25
Mateusz Ramotowski <mateusz.ramotowski.praca@gmail.com>
2022-03-23 16:28:00 +0100

    I programmed player move,and computer move. I also can choose size of grid to play. Next thing to do:  check when someone win the game.

---
+++ README.md
@@ -0,0 +1,66 @@
+<!-- finished:
+technologies used
+contact -->
+# Hangman-Game-Vanilia-JS
+> You can play hangman game in web browser.
+> Live demo [_here_](https://www.example.com). 
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Screenshots](#screenshots)
+* [Project Status](#project-status)
+* [Acknowledgements](#acknowledgements)
+* [Description of Code](#Description-of-Code)
+* [Contact](#contact)
+<!-- * [License](#license) -->
+
+
+## General Information
+
+
+## Technologies Used
+Project is created with:
+* HTML5
+* CSS3
+* JS(ES6+)
+
+I was using chrome developer tools for testing my application.
+## Features
+
+
+## Screenshots
+
+
+## Project Status
+
+
+## Acknowledgements
+
+
+## Description of Code
+When I click button to change grid:
+*V  grid area is rendered
+*V  ukryj wszystkie przyciski grid size (atrybut hidden)
+*V  pokaż przycisk play again (atrybut hidden)
+*V  dodaj EventListener dla każdego grid Box
+
+When I click button play again:
+*V  pokaż przyciski change grid size
+*V  ukryj przycisk play again
+*V  give freeGridBoxes początkowe elementy i długość
+
+When I click on Grid Box:
+*V  narysuj X na tym polu (gracz)
+*  sprawdź zwycięstwo 
+*V  zabierz event lestenera dla tego pola
+*V  zmień wartość tablicy freeGridBoxes
+*V  sprawdź czy są wolne miejsca w tablicy freeGridBoxes (Nullish coalescing operator (??))
+*V  komputer stawia 0 na losowym wolnym polu
+*  sprawdź zwycięstwo 
+*V  zabierz event lestenera dla tego pola
+*V  zmień wartość tablicy freeGridBoxes 
+
+## Contact
+Created by Mateusz Ramotowski (mateusz.ramotowski.praca@gmail.com) - feel free to contact me!
--- script.js
+++ script.js
@@ -8,39 +8,79 @@ from begging write clean code, do small code refactoring
 use chrome debugger for finding bugs*/
 const gridBoxes = document.getElementsByClassName("grid-box");
 const gridArea = document.getElementsByClassName("grid-area")[0];
-const buttonsChangeGridSize = document.getElementsByClassName("buttons-change-grid-size"); // there are three buttons
+const buttonsChangeGridSize = document.getElementsByClassName(
+  "buttons-change-grid-size"
+); // there are three buttons
 const buttonPLayAgain = document.getElementsByClassName("button-play-again")[0];
+let freeGridBoxes = [0, 1, 2, 3, 4, 5, 6, 7, 8]; // for 3x3 grid;
 
+function gridBoxFunctionality(event) {
+  let placeInFreeGridBoxes;
+  let randomNumber;
+  let doComputerMove;
+  let counterCheckingNullInFreeGridBoxes = 0;
+  event.target.innerText = "X";
+  // in event listener this mean DOM element that handler is attached to
+  this.removeEventListener("click", gridBoxFunctionality);
+  for (let i = 0; i < gridBoxes.length; i++) {
+    if (this === gridBoxes[i]) {
+      placeInFreeGridBoxes = i;
+      break;
+    }
+  }
+  freeGridBoxes[placeInFreeGridBoxes] = null;
+  console.log(placeInFreeGridBoxes);
+  console.log(freeGridBoxes);
+  // below checking free Grid boxes
+  for (let i = 0; i < freeGridBoxes.length; i++) {
+    let temp1 = freeGridBoxes[i] ?? "null inside";
+    if (temp1 === "null inside") {
+      counterCheckingNullInFreeGridBoxes++;
+    } else {
+      break;
+    }
+  }
+  if (counterCheckingNullInFreeGridBoxes !== freeGridBoxes.length) {
+    // below computer move
+    do {
+      randomNumber = Math.floor(Math.random() * freeGridBoxes.length);
+    } while (freeGridBoxes[randomNumber] === null);
+    freeGridBoxes[randomNumber] = null;
+    gridBoxes[randomNumber].innerText = "O";
+    gridBoxes[randomNumber].removeEventListener("click", gridBoxFunctionality);
+  }
+}
 function drawGridToPLayDOM(event) {
-  const numberOrRowsColumns = event.currentTarget.innerText[0]; //first char from clicked button 
+  const numberOrRowsColumns = event.currentTarget.innerText[0]; //first char from clicked button
   let innerHTML = "";
   for (let i = 0; i < numberOrRowsColumns; i++) {
-    innerHTML+='\n<div class="grid-row">\n'
+    innerHTML += '\n<div class="grid-row">\n';
     for (let i = 0; i < numberOrRowsColumns; i++) {
-      innerHTML+='  <div class="grid-box"></div>\n';
+      innerHTML += '  <div class="grid-box"></div>\n';
     }
-    innerHTML+='</div>\n';
+    innerHTML += "</div>\n";
   }
   gridArea.innerHTML = innerHTML;
-  for(let buttonChangeGridSize of buttonsChangeGridSize){
+  for (let buttonChangeGridSize of buttonsChangeGridSize) {
     buttonChangeGridSize.hidden = true;
   }
   buttonPLayAgain.hidden = false;
-  for(let gridBox of gridBoxes){
-    gridBox.addEventListener("click", function(event){
-      event.target.innerText = "X";
-
-    })
+  for (let i = 0; i < gridBoxes.length; i++) {
+    gridBoxes[i].addEventListener("click", gridBoxFunctionality);
   }
 }
 
-for(let buttonChangeGridSize of buttonsChangeGridSize){
+for (let buttonChangeGridSize of buttonsChangeGridSize) {
   buttonChangeGridSize.addEventListener("click", drawGridToPLayDOM);
 }
-buttonPLayAgain.addEventListener("click", function(){
-  for(let buttonChangeGridSize of buttonsChangeGridSize){
+buttonPLayAgain.addEventListener("click", function () {
+  for (let buttonChangeGridSize of buttonsChangeGridSize) {
     buttonChangeGridSize.hidden = false;
   }
   buttonPLayAgain.hidden = true;
-})
-
+  freeGridBoxes.length = 9;
+  for (let i = 0; i < freeGridBoxes.length; i++) {
+    freeGridBoxes[i] = i;
+  }
+  console.log(freeGridBoxes);
+});
